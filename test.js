99bffb1fba2269bd7de047e9f298810aa267c0a7
paulmolin42 <paulmp.molin@gmail.com>
2017-03-01 08:16:59 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+  let previous = 0;
+  let current = 1;
+
+  for(let i = 0; i < n; i += 1) {
+    let temp = previous;
+    previous = current;
+    current = temp + current;
+  }
+
+  return current;
+}
